aae36bf80c485f6f1df4cf8eec29f1e5e8041dd4
julyfun <43675484+julyfun@users.noreply.github.com>
2023-12-18 22:17:14 +0800

    feat: 5.1 构建 Login 页面

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@/App'
+import App from '@/pages/login'
 import '@/common/styles/frame.styl'
 import { ConfigProvider } from 'antd'
 import zhCN from 'antd/es/locale/zh_CN'
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,26 @@
+import { Button, Input } from 'antd'
+import imgLogo from './logo.png'
+import './login.styl'
+
+function Login() {
+    return (
+        <div className="P-login">
+            <img src={imgLogo} alt="" className="logo" />
+            <div className="ipt-con">
+                <Input placeholder="账号" />
+            </div>
+            <div className="ipt-con">
+                <Input.Password placeholder="密码" />
+            </div>
+            <div className="ipt-con">
+                <Button type="primary" block={true}>
+                    登录
+                </Button>
+            </div>
+        </div>
+
+    )
+}
+
+
+export default Login
